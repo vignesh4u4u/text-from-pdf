883ae7584ae325dd0f48452c1f333d246e01a9d0
vicky4u4u <vignesh.subramani@yorosis.com>
2023-07-27 13:35:50 +0530

    postman API

--- tik.py
+++ tik.py
@@ -47,24 +47,25 @@ def extract_address_custom(text):
 def format_addresses(addresses):
     formatted_addresses = {}
     for key, address in addresses.items():
-        formatted_address = address.replace('\u2022', ',')
+        formatted_address = address.replace('\u2022', '').strip()
         formatted_addresses[key] = formatted_address
     return formatted_addresses
-@app.route("/")
-def home():
-    return render_template("che.html")
-@app.route("/pre", methods=["POST", "GET"])
+
+@app.route("/ml-service/text-extraction", methods=["POST"])
 def text_from_pdf():
     if request.method == 'POST':
-        file = request.files['pdf_file']
-        selected_options = request.form.getlist('myselect')
+        file = request.files['files']
+        selected_options = request.form["extractOptions"]
         file_path = "temp.pdf"
         file.save(file_path)
-        with open(file_path, 'rb') as f: # f=file_path.read()
+        with open(file_path, 'rb') as f:
             text = extract_text(f)
+
         data = {}
+
         if "full_text" in selected_options:
             data['full_text'] = text
+
         if "addresses" in selected_options:
             addresse1 = pyap.parse(text, country="US")
             addresse2 = pyap.parse(text, country="GB")  # UK
@@ -73,11 +74,16 @@ def text_from_pdf():
             address = extract_address_custom(text)
             all_addresses = {}
             if address:
-                all_addresses['custom_address'] = address
-            if addresses :
+                all_addresses['Custom Address'] = address
+            if addresses:
                 unique_addresses = list(set(address.full_address for address in addresses))
-                all_addresses.update({f"address_{idx}": address for idx, address in enumerate(unique_addresses, start=1)})
-            data['addresses'] = format_addresses(all_addresses)
+                unique_addresses.sort()  # Sort the addresses for consistency
+                all_addresses.update({f"Address_{idx}": address for idx, address in enumerate(unique_addresses, start=1)})
+            data['addresses'] = {
+                "address_count": len(all_addresses),
+                "addresses": [{"value": address} for address in all_addresses.values()]
+            }
+
         if "dates" in selected_options:
             date_pattern = r'(?i)\b(?:\d{1,2}[-/]\d{1,2}[-/]\d{2,4}|' \
                            r'\d{1,2}(?:st|nd|rd|th)? \w+ \d{2,4}|' \
@@ -90,11 +96,12 @@ def text_from_pdf():
             matches = re.findall(date_pattern, text, flags=re.IGNORECASE)
             dates = [parser.parse(match, fuzzy=True) for match in matches]
             unique_dates = list(set(date.strftime("%Y-%m-%d") for date in dates))
-            ordered_dates_dict = OrderedDict()
-            for idx, date in enumerate(unique_dates, start=1):
-                ordered_dates_dict[f"date_{idx}"] = date
-            data['dates'] = ordered_dates_dict
-            data['date_count'] = len(ordered_dates_dict)
+            unique_dates.sort()  # Sort the dates for consistency
+            ordered_dates_list = [{"value": date} for date in unique_dates]
+            data['dates'] = {
+                "date_count": len(ordered_dates_list),
+                "dates": ordered_dates_list
+            }
 
         if "names" in selected_options:
             def extract_names_from_pdf(file_path):
@@ -111,15 +118,19 @@ def text_from_pdf():
                         unique_names = set(ent.text for ent in doc.ents if ent.label_ == 'PERSON')
                         names.update(unique_names)
                 return names
+
             extracted_names = extract_names_from_pdf(file_path)
             name_length_threshold = 25
             filtered_names = [name for name in extracted_names if len(name) <= name_length_threshold]
-            formatted_names = {f"name_{idx}": name for idx, name in enumerate(filtered_names, start=1)}
+            formatted_names = [{"value": name} for name in filtered_names]
+            formatted_names.sort(key=lambda x: x["value"])  # Sort the names for consistency
             data['names'] = {
-                'name_count': len(filtered_names),
-                'names': formatted_names}
-        os.remove(file_path)
+                "name_count": len(filtered_names),
+                "names": formatted_names
+            }
+
         return jsonify(data)
-    return render_template("che.html", **locals())
+
+    #return render_template("che.html", **locals())
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
