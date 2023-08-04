60f359226bf935d2352f8293d82767cda26cb7a7
Vignesh S <128770770+vignesh4u4u@users.noreply.github.com>
2023-08-04 15:26:32 +0530

    Add files via upload

---
+++ NERMETHOD.py
@@ -0,0 +1,183 @@
+from flask import Flask, request, render_template, jsonify
+from pdfminer.high_level import extract_text, extract_pages, extract_text_to_fp
+#from simpletransformers.ner import NERModel,NERArgs
+#from sklearn.preprocessing import LabelEncoder,StandardScaler
+import pickle as pkl
+import json
+import pyap
+import PyPDF2
+import pandas as pd
+import numpy as np
+from dateutil import parser
+import datefinder
+import dateparser
+import nltk
+import re
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import pdfplumber
+import spacy
+from dateparser.search import search_dates
+from nameparser import HumanName
+import os
+import addressparser
+from collections import OrderedDict
+app = Flask(__name__)
+nltk.download('punkt')
+nltk.download('stopwords')
+nlp = spacy.load('en_core_web_sm')
+#read=pkl.load(open("NERBE.pkl","rb"))
+def extract_address_custom(text):
+    address_line_pattern = r"\d+ [\w\s.,]+"
+    suite_pattern = r"Suite [\w\d]+"
+    city_state_zip_pattern = r"[A-Z][A-Za-z\s]+, [A-Z]{2} \d{5}"
+    address_line_match = re.search(address_line_pattern, text)
+    suite_match = re.search(suite_pattern, text)
+    city_state_zip_match = re.search(city_state_zip_pattern, text)
+    if address_line_match and city_state_zip_match:
+        address_line = address_line_match.group()
+        suite = suite_match.group().replace("Suite ", "") if suite_match else None
+        city_state_zip = city_state_zip_match.group()
+        address = f"{address_line}{', ' + suite if suite else ''} • {city_state_zip}"
+        unwanted_text = "1 of 81. PARTIES. This Lease Contract"
+        address = address.replace(unwanted_text, "")
+        return address.strip()
+    else:
+        return None
+def format_addresses(addresses):
+    formatted_addresses = {}
+    for key, address in addresses.items():
+        formatted_address = address.replace('\u2022', ',')
+        formatted_addresses[key] = formatted_address
+    return formatted_addresses
+@app.route("/ml-service/health/v1/ping",methods=["GET"])
+def home():
+    if request.method == 'GET':
+        return "pong"
+@app.route("/ml-service/text-extraction", methods=["POST"])
+def text_from_pdf():
+    if request.method == 'POST':
+        #print(request.files)
+        file = request.files["files"]
+        selected_options = request.form["extractOptions"]
+        file_path = "temp.pdf"
+        file.save(file_path)
+        with open(file_path, 'rb') as f:
+            text = extract_text(f)
+        data = {}
+        if "addresses" in selected_options:
+            addresse1 = pyap.parse(text, country="US")
+            addresse2 = pyap.parse(text, country="GB")  # uk
+            addresse3 = pyap.parse(text, country="CA")  # canada
+            addresses = addresse1
+            filtered_addresses = [address.full_address for address in addresses if
+                                  "1 RESIDENT IS RESPONSIBLE FOR CHAR" not in address.full_address
+                                  and "invitees.11. THERE IS NO WARRANTY OF A SMOK" not in address.full_address]
+            addresses = addresse1
+            address = extract_address_custom(text)
+            addresses = {}
+            if address:
+                addresses['custom_address'] = address
+            if filtered_addresses:
+                unique_addresses = list(set(filtered_addresses))
+                addresses.update({f"address_{idx}": addr for idx, addr in enumerate(unique_addresses, start=1)})
+                data['addresses'] = addresses
+                data['address_count'] = len(addresses)
+            else:
+                data["address_response"] = "No addresses found"
+
+        if "dates" in selected_options:
+            date_pattern = r'(?i)\b(?:\d{1,2}[-/]\d{1,2}[-/]\d{2,4}|' \
+                           r'\d{1,2}(?:st|nd|rd|th)? \w+ \d{2,4}|' \
+                           r'\d{1,2} \w+ \d{2,4}|' \
+                           r'(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* \d{1,2},? \d{2,4}|' \
+                           r'(?:jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|nov(?:ember)?|dec(?:ember)?) \d{1,2}, \d{4}|' \
+                           r'[a-zA-Z]{3} \d{1,2}, \d{4}|' \
+                           r'[a-zA-Z]{3} \d{1,2},\d{4})\b'
+
+            matches = re.findall(date_pattern, text, flags=re.IGNORECASE)
+            dates = [parser.parse(match, fuzzy=True) for match in matches]
+            unique_dates = list(set(date.strftime("%Y-%m-%d") for date in dates))
+            # Filter out dates with starting year "0"
+            valid_dates = [date for date in unique_dates if not date.startswith("0")]
+            ordered_dates_dict = OrderedDict()
+            for idx, date in enumerate(valid_dates, start=1):
+                ordered_dates_dict[f"date_{idx}"] = date
+            data['dates'] = ordered_dates_dict
+            data['date_count'] = len(ordered_dates_dict)
+
+        if "names" in selected_options:
+            def extract_names_from_pdf(file_path):
+                names = set()
+                stop_words = set(stopwords.words('english'))
+                with pdfplumber.open(file_path) as pdf:
+                    for page in pdf.pages:
+                        page_text = page.extract_text()
+                        tokens = word_tokenize(page_text)
+                        filtered_tokens = [token for token in tokens if token.isalpha() and token.lower() not in stop_words]
+                        text = ' '.join(filtered_tokens)
+                        doc = nlp(text)
+                        unique_names = set(ent.text for ent in doc.ents if ent.label_ == 'PERSON')
+                        names.update(unique_names)
+                return names
+            extracted_names = extract_names_from_pdf(file_path)
+            name_length_threshold = 25
+            filtered_names = [name for name in extracted_names if (len(name) <= name_length_threshold and len(name) > 2)]
+            formatted_names = {f"name_{idx}": name for idx, name in enumerate(filtered_names, start=1)}
+            data['names'] = formatted_names
+            data['name_count'] = len(formatted_names)
+
+        if "human_names" in selected_options:
+            def remove_punctuation(text):
+                cleaned_text = re.sub(r'[^\w\s]', '', text)
+                return cleaned_text
+            #text = extract_text('/kaggle/input/lease6/Lease (6).pdf')
+            cleaned_text = remove_punctuation(text)
+            """result, raw_outputs = read.predict([cleaned_text])
+            filtered_name = []
+            for tokens in result[0]:
+                for name, label in tokens.items():
+                    if label == 'I-PER' or label == 'B-PER':
+                        filtered_name.append(name)            
+            data['name_count'] = len(filtered_name)
+            data['names'] = [{"value": name} for name in filtered_name]"""
+
+
+        if "full_text" in selected_options:
+            data['full_text'] = text
+
+        def find_usd_amount(text):
+            usd_pattern = r'\$\s*(\d+(\.\d+)?)'
+            matches = re.findall(usd_pattern, text)
+            usd_amounts = [float(match[0]) for match in matches]
+            return usd_amounts
+
+        if "monetary_amounts" in selected_options:
+            usd_amounts = find_usd_amount(text)
+            formatted_amounts = {f"amount_{idx}": amount for idx, amount in enumerate(usd_amounts, start=1)}
+            data['monetary_amounts'] = formatted_amounts
+            data['monetary_amount_count'] = len(formatted_amounts)
+
+        """if "monetary_amounts" in selected_options:
+            doc = nlp(text)
+            us_amounts = [ent.text for ent in doc.ents if ent.label_ == "MONEY"]
+            formatted_amounts = {f"amount_{idx}": amount for idx, amount in enumerate(us_amounts, start=1)}
+            data['monetary_amounts'] = formatted_amounts
+            data['monetary_amount_count'] = len(formatted_amounts)
+
+        if "emails" in selected_options:
+            email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            emails = re.findall(email_pattern, text)
+            formatted_emails = [{"value": email} for email in emails]
+            data['emails'] = formatted_emails
+            data['email_count'] = len(formatted_emails)"""
+
+        os.remove(file_path)
+        return jsonify(data)
+    #return render_template("che.html", **locals())
+    #host="10.244.0.7",port=8080
+    #http://localhost:8080/ml-service/text-extraction
+    #https://india.yoroflow.com/ml-service/text-extraction
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=8080)
